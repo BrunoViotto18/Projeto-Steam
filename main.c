f2d494fd2cf8cf13cbd07a0ccccd8c9f4bb5ae36
Bruno Viotto <brunoviottosantos@gmail.com>
2021-11-03 11:43:58 -0300

    Criação da biblioteca/structs e comço da criação do menu.

--- main.c
+++ main.c
@@ -3,6 +3,45 @@
 
 int main()
 {
-    printf("Hello world!\n");
+    bool con = true;
+    int op;
+    while (con)
+    {
+        printf(
+        "MENU\n" +
+        "[ 1 ] Listar\n" +
+        "[ 2 ] Adiconar\n" +
+        "[ 3 ] Remover\n" +
+        "[ 4 ] Sair");
+        scanf("%d", &op)
+
+        switch (op)
+        {
+            case 1:
+                printf(
+                "MENU\n" +
+                "[ 1 ] Jogo\n" +
+                "[ 2 ] Publisher\n" +
+                "[ 3 ] User\n" +
+                "[ 4 ] Voltar ao Menu");
+                scanf("%d", &op)
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            case 4:
+                con = false;
+                break;
+
+            default:
+                printf("Opção inválida! Tente novamente.");
+                break;
+        }
+    }
+
     return 0;
 }
